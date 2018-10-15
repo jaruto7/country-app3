5775181cd0542cd65512e8a9cee6c3304d58a65a
Jarek Kajko <jaruto7@gmail.com>
2018-10-15 21:09:26 +0200

    Add routing to the app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import DevTools from './DevTools';
+import routes from './routes';
+// import DevTools from './DevTools';
+
 
 render(
     <Provider store={store}>
-        <div>
+        <Router history={hashHistory} routes={routes}/>
+        {/* <div>
             <h1>Inicjalizacja projektu</h1>
             <DevTools />
-        </div>
+        </div> */}
     </Provider>,
     document.getElementById('root')
 );
